b3832b653b410982a96f5f5d6d409e05a3dd48e3
Shivam Agrawal <sa9598326@gmail.com>
2026-04-01 14:32:26 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Admin from "./pages/Admin";
